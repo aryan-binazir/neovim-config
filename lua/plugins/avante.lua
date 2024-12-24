b1ddb5c7dfab426ca95ecf0ff456ad08ed64c4a2
Aryan Binazir <abinazir@gmail.com>
2024-12-23 22:06:15 -0500

    fix: simplify avante.nvim config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,37 +1,10 @@
 return {
     "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false,
-    build = "make BUILD_FROM_SOURCE=true",
     dependencies = {
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
-        "hrsh7th/nvim-cmp",
+        "nvim-lua/plenary.nvim",
+        "stevearc/dressing.nvim",
         "nvim-tree/nvim-web-devicons",
-        "zbirenbaum/copilot.lua",
-        {
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-                default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = {
-                        insert_mode = true,
-                    },
-                    use_absolute_path = true,
-                },
-            },
-        },
-        {
-            'MeanderingProgrammer/render-markdown.nvim',
-            opts = {
-                file_types = { "markdown", "Avante" },
-            },
-            ft = { "markdown", "Avante" },
-        },
     },
     config = function()
         require("avante").setup({})
