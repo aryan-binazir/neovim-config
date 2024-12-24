5495e20bfb5a181cd57e412cfff6d7bd9ebac51e
Aryan Binazir <abinazir@gmail.com>
2024-12-24 06:41:58 -0500

    refactor: improve completion configuration\n\n- Add lazy loading on InsertEnter\n- Add regex support for snippets\n- Change confirm to C-s\n- Use C-l/C-h for snippet navigation\n- Simplify mappings

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,7 +1,16 @@
 return {
     'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
     dependencies = {
-        'L3MON4D3/LuaSnip',
+        {
+            'L3MON4D3/LuaSnip',
+            build = (function()
+                if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                    return
+                end
+                return 'make install_jsregexp'
+            end)(),
+        },
         'saadparwaiz1/cmp_luasnip',
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-path',
@@ -11,7 +20,7 @@ return {
         local cmp = require 'cmp'
         local luasnip = require 'luasnip'
         require('luasnip.loaders.from_vscode').lazy_load()
-        luasnip.config.setup({})
+        luasnip.config.setup {}
 
         cmp.setup {
             snippet = {
@@ -19,35 +28,22 @@ return {
                     luasnip.lsp_expand(args.body)
                 end,
             },
-            completion = {
-                completeopt = 'menu,menuone,noinsert',
-            },
+            completion = { completeopt = 'menu,menuone,noinsert' },
             mapping = cmp.mapping.preset.insert {
                 ['<C-n>'] = cmp.mapping.select_next_item(),
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete {},
-                ['<CR>'] = cmp.mapping.confirm {
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                },
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_locally_jumpable() then
+                ['<C-s>'] = cmp.mapping.confirm { select = true },
+                ['<C-l>'] = cmp.mapping(function()
+                    if luasnip.expand_or_locally_jumpable() then
                         luasnip.expand_or_jump()
-                    else
-                        fallback()
                     end
                 end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
+                ['<C-h>'] = cmp.mapping(function()
+                    if luasnip.locally_jumpable(-1) then
                         luasnip.jump(-1)
-                    else
-                        fallback()
                     end
                 end, { 'i', 's' }),
             },
