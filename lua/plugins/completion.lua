7b0db287a447ebf1e9e8e5fdc06584d6b6a3d62b
Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
2024-12-24 10:48:27 -0500

    Update completion.lua

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -42,7 +42,7 @@ return {
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete {},
-                ['<C-y>'] = cmp.mapping.confirm { select = true },
+                ['<CR>'] = cmp.mapping.confirm { select = true },
                 ['<C-e>'] = cmp.mapping.abort(),
             },
             sources = {
