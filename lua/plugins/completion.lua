552eacc5d513e38b1a6b24ac7c287dcebd736ce4
Aryan Binazir <abinazir@gmail.com>
2024-12-24 07:39:15 -0500

    refactor: remove C-n/C-p from cmp\n\n- Remove C-n/C-p from cmp to avoid conflicts with copilot\n- Keep C-y for confirmation

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -36,9 +36,7 @@ return {
             },
             completion = { completeopt = 'menu,menuone,noinsert' },
             mapping = cmp.mapping.preset.insert {
-                ['<C-n>'] = cmp.mapping.select_next_item(),
                 ['<C-j>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
                 ['<C-k>'] = cmp.mapping.select_prev_item(),
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
