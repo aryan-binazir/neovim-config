6c2b501addacc3f1ab0f6098610d890aea526532
Aryan Binazir <abinazir@gmail.com>
2024-12-24 07:51:16 -0500

    Completion changes

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -42,16 +42,7 @@ return {
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete {},
                 ['<C-y>'] = cmp.mapping.confirm { select = true },
-                ['<C-l>'] = cmp.mapping(function()
-                    if luasnip.expand_or_locally_jumpable() then
-                        luasnip.expand_or_jump()
-                    end
-                end, { 'i', 's' }),
-                ['<C-h>'] = cmp.mapping(function()
-                    if luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    end
-                end, { 'i', 's' }),
+                ['<C-e>'] = cmp.mapping.abort(),
             },
             sources = {
                 { name = 'nvim_lsp' },
