9aa3ecf31d25c57eeb76dd8d773af79a81539661
Aryan Binazir <abinazir@gmail.com>
2024-12-24 08:23:53 -0500

    feat: simplify which-key setup\n\n- Remove all custom configuration\n- Set timeoutlen to 2000ms (2 seconds)\n- Use default which-key settings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,43 +1,8 @@
 return {
     'folke/which-key.nvim',
     config = function()
-        vim.opt.timeoutlen = 1000  -- Time to wait for a mapped sequence to complete (in milliseconds)
+        vim.opt.timeoutlen = 2000  -- Wait 2 seconds for key sequences
 
-        local wk = require("which-key")
-        
-        -- Setup which-key
-        wk.setup({
-            plugins = {
-                presets = {
-                    operators = false,
-                    motions = false,
-                    text_objects = false,
-                    windows = false,
-                    nav = false,
-                    z = false,
-                    g = false,
-                },
-            },
-        spec = {
-            { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-            { '<leader>d', group = '[D]ocument' },
-            { '<leader>r', group = '[R]ename' },
-            { '<leader>s', group = '[S]earch' },
-            { '<leader>w', group = '[W]orkspace' },
-            { '<leader>t', group = '[T]oggle' },
-            { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-          },
-
-        -- Register groups
-        -- wk.register({
-        --     ["<leader>c"] = { name = "+[C]opilot" },
-        --     ["<leader>d"] = { name = "+[D]ebug/Diagnostics" },
-        --     ["<leader>f"] = { name = "+[F]ind/Files" },
-        --     ["<leader>h"] = { name = "+Git [H]unk" },
-        --     ["<leader>l"] = { name = "+[L]SP/Lazy" },
-        --     ["<leader>s"] = { name = "+[S]earch" },
-        --     ["<leader>t"] = { name = "+[T]oggle" },
-        --     ["<leader>w"] = { name = "+[W]orkspace" },
-        -- })
-    -- end
+        require("which-key").setup()
+    end
 } 
