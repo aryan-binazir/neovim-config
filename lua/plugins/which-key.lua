89d1be05d1b5d54c9fbc0bb304504f89156b4c37
Aryan Binazir <abinazir@gmail.com>
2024-12-23 21:26:48 -0500

    refactor: simplify which-key configuration\n\n- Remove toggle functionality\n- Keep basic setup with disabled presets\n- Improve stability

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,8 +1,6 @@
 return {
     'folke/which-key.nvim',
     config = function()
-        -- Disable which-key by default
-        vim.g.which_key_enabled = false
         require("which-key").setup({
             plugins = {
                 presets = {
@@ -15,23 +13,6 @@ return {
                     g = false,
                 },
             },
-            -- Start in disabled mode
-            disable = {
-                buftypes = {},
-                filetypes = {},
-            },
         })
-
-        -- Add toggle keymap
-        vim.keymap.set('n', '<leader>tw', function()
-            vim.g.which_key_enabled = not vim.g.which_key_enabled
-            if vim.g.which_key_enabled then
-                require("which-key").setup({ disable = { buftypes = {}, filetypes = {} } })
-                vim.notify("Which-key enabled")
-            else
-                require("which-key").setup({ disable = { buftypes = { ".*" }, filetypes = { ".*" } } })
-                vim.notify("Which-key disabled")
-            end
-        end, { desc = "Toggle which-key" })
     end
 } 
