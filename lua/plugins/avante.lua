207abf460b5c3677ecc50bc2b32c18ed5a7c27f0
Aryan Binazir <abinazir@gmail.com>
2024-12-23 21:52:02 -0500

    fix: add nui.nvim dependency for avante.nvim

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,6 +1,9 @@
 return {
     "yetone/avante.nvim",
     event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
     config = function()
         require("avante").setup({})
         
