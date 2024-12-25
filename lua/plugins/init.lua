32821a5903185ff986e9e85b01dfdfb6c4df1162
Aryan Binazir <abinazir@gmail.com>
2024-12-24 19:57:45 -0500

    feat: switch to catppuccin theme\n\n- Remove tokyonight theme\n- Configure catppuccin-mocha as default theme\n- Add config function for theme setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -64,11 +64,11 @@ return {
 
     -- Theme
     {
-        "folke/tokyonight.nvim",
-        lazy = false,
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
         config = function()
-            vim.cmd [[colorscheme tokyonight-night]]
+            vim.cmd.colorscheme "catppuccin-mocha"
         end,
     },
 
