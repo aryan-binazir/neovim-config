7c25c2facc597a19213e9e65f1c1e6f0e34d26b9
Aryan Binazir <abinazir@gmail.com>
2024-12-25 19:53:22 -0500

    refactor(harpoon): replace keybindings with numerical system

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,13 +9,19 @@ return {
         -- vim.keymap.set("n", "<C-t>", function() toggle_telescope(harpoon:list()) end,
         --   { desc = "Open harpoon window" })
         vim.keymap.set("n", "<leader>m", function() harpoon:list():append() end, { desc = 'Add to Harpoon' })
-        -- vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end)
+        -- vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end, { desc = 'Add to Harpoon' })
         vim.keymap.set("n", "<C-g>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-q>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-w>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-a>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+        -- Change to number keys 1-9
+        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
+        vim.keymap.set("n", "<C-6>", function() harpoon:list():select(6) end)
+        vim.keymap.set("n", "<C-7>", function() harpoon:list():select(7) end)
+        vim.keymap.set("n", "<C-8>", function() harpoon:list():select(8) end)
+        vim.keymap.set("n", "<C-9>", function() harpoon:list():select(9) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
