aac2877407be308cd43f937f6fcc4b3b58d9af3b
Aryan Binazir <abinazir@gmail.com>
2024-12-25 20:10:29 -0500

    remove: which-key plugin

--- lua/plugins/which-key.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'folke/which-key.nvim',
-    config = function()
-        vim.opt.timeoutlen = 2000  -- Wait 2 seconds for key sequences
-
-        require("which-key").setup()
-    end
-} 
