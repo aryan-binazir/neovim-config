ba832fd80a6d3ef25d61a57b714f238ca7b57ec8
Aryan Binazir <abinazir@gmail.com>
2024-12-25 20:05:22 -0500

    refactor(harpoon): replace ctrl+number with leader+number keybindings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,22 +6,19 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        -- vim.keymap.set("n", "<C-t>", function() toggle_telescope(harpoon:list()) end,
-        --   { desc = "Open harpoon window" })
         vim.keymap.set("n", "<leader>m", function() harpoon:list():append() end, { desc = 'Add to Harpoon' })
-        -- vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end, { desc = 'Add to Harpoon' })
         vim.keymap.set("n", "<C-g>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        -- Change to number keys 1-9
-        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
-        vim.keymap.set("n", "<C-6>", function() harpoon:list():select(6) end)
-        vim.keymap.set("n", "<C-7>", function() harpoon:list():select(7) end)
-        vim.keymap.set("n", "<C-8>", function() harpoon:list():select(8) end)
-        vim.keymap.set("n", "<C-9>", function() harpoon:list():select(9) end)
+        -- Change to leader + number keys
+        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+        vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+        vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+        vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
+        vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
