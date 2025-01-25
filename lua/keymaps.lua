044b0a7a2fd05763fa2634eb94807cb3b3523e92
ar <aryanbinazir@mac-mini.lan>
2025-01-25 17:39:26 -0500

    Goto error optimizations

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,13 +15,14 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<A-k>', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', '<A-j>', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Codeium keymaps
-vim.keymap.set('n', '<leader>ta', ':lua toggle_codeium_ghost_text()<CR>', { noremap = true, silent = true, desc = 'Toggle Codeium ghost text' })
+vim.keymap.set('n', '<leader>ta', ':lua toggle_codeium_ghost_text()<CR>',
+    { noremap = true, silent = true, desc = 'Toggle Codeium ghost text' })
 
 -- Other keymaps
 vim.keymap.set("n", "<leader>cc", ":Codeium Chat<CR>", { noremap = true, silent = true, desc = "Open Codeium Chat" })
@@ -35,6 +36,19 @@ vim.keymap.set('n', '<leader>sl', function() vim.cmd("Sleuth") end, { desc = '[S
 -- Quick exit keymaps
 vim.keymap.set('n', '<leader>qq', ':q!<CR>')
 vim.keymap.set('n', '<leader>wq', ':wq!<CR>')
+
+-- Location list specific mappings
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'qf',
+    callback = function()
+        vim.keymap.set('n', '<CR>', function()
+            -- Get the current line number which represents the location entry
+            local line = vim.api.nvim_win_get_cursor(0)[1]
+            -- Execute the location list jump
+            vim.cmd(line .. 'll')
+        end, { buffer = true, noremap = true, desc = 'Jump to location list item' })
+    end,
+})
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
