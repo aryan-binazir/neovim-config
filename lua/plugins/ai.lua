b1f11dcd10040341625200e918d6e80d6cc47c29
Aryan Binazir <abinazir@gmail.com>
2025-02-08 10:37:40 -0500

    Add Av

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,82 +1,4 @@
 return {
-  -- {
-  --   "Exafunction/codeium.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "hrsh7th/nvim-cmp",
-  --   },
-  --   config = function()
-  --     require("codeium").setup({
-  --       -- Optionally disable cmp source if using virtual text only
-  --       enable_cmp_source = false,
-  --       virtual_text = {
-  --         enabled = true,
-
-  --         -- These are the defaults
-
-  --         -- Set to true if you never want completions to be shown automatically.
-  --         manual = false,
-  --         -- A mapping of filetype to true or false, to enable virtual text.
-  --         filetypes = {},
-  --         -- Whether to enable virtual text of not for filetypes not specifically listed above.
-  --         default_filetype_enabled = true,
-  --         -- How long to wait (in ms) before requesting completions after typing stops.
-  --         idle_delay = 75,
-  --         -- Priority of the virtual text. This usually ensures that the completions appear on top of
-  --         -- other plugins that also add virtual text, such as LSP inlay hints, but can be modified if
-  --         -- desired.
-  --         virtual_text_priority = 65535,
-  --         -- Set to false to disable all key bindings for managing completions.
-  --         map_keys = true,
-  --         -- The key to press when hitting the accept keybinding but no completion is showing.
-  --         -- Defaults to \t normally or <c-n> when a popup is showing.
-  --         accept_fallback = nil,
-  --         -- Key bindings for managing completions in virtual text mode.
-  --         key_bindings = {
-  --           -- Accept the current completion.
-  --           accept = "<S-Tab>",
-  --           -- Accept the next word.
-  --           accept_word = false,
-  --           -- Accept the next line.
-  --           accept_line = "<C-l>",
-  --           -- Clear the virtual text.
-  --           clear = "<C-x>",
-  --           -- Cycle to the next completion.
-  --           next = "<C-j>",
-  --           -- Cycle to the previous completion.
-  --           prev = "<C-k>",
-  --         }
-  --       }
-  --     })
-
-  --     -- Create a global function to toggle ghost text
-  --     vim.g.codeium_ghost_text_enabled = true
-  --     _G.toggle_codeium_ghost_text = function()
-  --       vim.g.codeium_ghost_text_enabled = not vim.g.codeium_ghost_text_enabled
-  --       require("codeium").setup({
-  --         enable_cmp_source = false,
-  --         virtual_text = {
-  --           enabled = vim.g.codeium_ghost_text_enabled,
-  --           manual = false,
-  --           filetypes = {},
-  --           default_filetype_enabled = true,
-  --           idle_delay = 75,
-  --           virtual_text_priority = 65535,
-  --           map_keys = true,
-  --           accept_fallback = nil,
-  --           key_bindings = {
-  --             accept = "<S-Tab>",
-  --             accept_word = false,
-  --             accept_line = "<C-l>",
-  --             clear = "<C-x>",
-  --             next = "<C-j>",
-  --             prev = "<C-k>",
-  --           }
-  --         }
-  --       })
-  --     end
-  --   }
-  -- },
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
@@ -96,6 +18,128 @@ return {
       })
     end,
   },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "copilot",
+      copilot = {
+        model = "claude-3.5-sonnet",
+      },
+    },
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "muniftanjim/nui.nvim",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+      "ibhagwan/fzf-lua",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        "hakonharnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        'meanderingprogrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "avante" },
+        },
+        ft = { "markdown", "avante" },
+      },
+    },
+  },
+  -- {
+  --   "exafunction/codeium.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "hrsh7th/nvim-cmp",
+  --   },
+  --   config = function()
+  --     require("codeium").setup({
+  --       -- optionally disable cmp source if using virtual text only
+  --       enable_cmp_source = false,
+  --       virtual_text = {
+  --         enabled = true,
+  --
+  --         -- these are the defaults
+  --
+  --         -- set to true if you never want completions to be shown automatically.
+  --         manual = false,
+  --         -- a mapping of filetype to true or false, to enable virtual text.
+  --         filetypes = {},
+  --         -- whether to enable virtual text of not for filetypes not specifically listed above.
+  --         default_filetype_enabled = true,
+  --         -- how long to wait (in ms) before requesting completions after typing stops.
+  --         idle_delay = 75,
+  --         -- priority of the virtual text. this usually ensures that the completions appear on top of
+  --         -- other plugins that also add virtual text, such as lsp inlay hints, but can be modified if
+  --         -- desired.
+  --         virtual_text_priority = 65535,
+  --         -- set to false to disable all key bindings for managing completions.
+  --         map_keys = true,
+  --         -- the key to press when hitting the accept keybinding but no completion is showing.
+  --         -- defaults to \t normally or <c-n> when a popup is showing.
+  --         accept_fallback = nil,
+  --         -- key bindings for managing completions in virtual text mode.
+  --         key_bindings = {
+  --           -- accept the current completion.
+  --           accept = "<s-tab>",
+  --           -- accept the next word.
+  --           accept_word = false,
+  --           -- accept the next line.
+  --           accept_line = "<c-l>",
+  --           -- clear the virtual text.
+  --           clear = "<c-x>",
+  --           -- cycle to the next completion.
+  --           next = "<c-j>",
+  --           -- cycle to the previous completion.
+  --           prev = "<c-k>",
+  --         }
+  --       }
+  --     })
+  --
+  --     -- create a global function to toggle ghost text
+  --     vim.g.codeium_ghost_text_enabled = true
+  --     _g.toggle_codeium_ghost_text = function()
+  --       vim.g.codeium_ghost_text_enabled = not vim.g.codeium_ghost_text_enabled
+  --       require("codeium").setup({
+  --         enable_cmp_source = false,
+  --         virtual_text = {
+  --           enabled = vim.g.codeium_ghost_text_enabled,
+  --           manual = false,
+  --           filetypes = {},
+  --           default_filetype_enabled = true,
+  --           idle_delay = 75,
+  --           virtual_text_priority = 65535,
+  --           map_keys = true,
+  --           accept_fallback = nil,
+  --           key_bindings = {
+  --             accept = "<s-tab>",
+  --             accept_word = false,
+  --             accept_line = "<c-l>",
+  --             clear = "<c-x>",
+  --             next = "<c-j>",
+  --             prev = "<c-k>",
+  --           }
+  --         }
+  --       })
+  --     end
+  --   }
+  -- },
   -- {
   --   "CopilotC-Nvim/CopilotChat.nvim",
   --   dependencies = {
