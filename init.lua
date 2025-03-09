b80127023a0f94e9ad5c4dbe729d1660f216237f
Aryan Binazir <abinazir@gmail.com>
2025-03-09 11:59:23 -0400

    Disable copilot by default

--- init.lua
+++ init.lua
@@ -14,6 +14,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Ensure Copilot starts disabled, enable when needed
+vim.g.copilot_enabled = false
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        vim.defer_fn(function()
+            vim.cmd("Copilot disable")
+        end, 200)
+    end,
+})
+
 require('lazy').setup({
     spec = {
         { import = "plugins" },
