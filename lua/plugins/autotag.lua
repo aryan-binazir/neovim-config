e45638fc3d68907b37a112ab67ad4dd8f969b554
Aryan Binazir <abinazir@gmail.com>
2025-07-20 19:19:37 -0400

    Another update

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -5,35 +5,15 @@ return {
         "nvim-treesitter/nvim-treesitter",
     },
     opts = {
-        enable_close = true,
-        enable_rename = true,
-        enable_close_on_slash = true,
-        filetypes = {
-            -- HTML and XML
-            'html',
-            'xml',
-            
-            -- JavaScript/TypeScript
-            'javascript',
-            'typescript',
-            
-            -- JSX/TSX files
-            'javascriptreact', -- .jsx files
-            'typescriptreact', -- .tsx files
-            'jsx',
-            'tsx',
-            
-            -- Markdown (for HTML in markdown)
-            'markdown',
-            
-            -- Other supported filetypes (commented for reference)
-            -- 'vue',         -- Vue components
-            -- 'svelte',      -- Svelte components
-            -- 'php',         -- PHP with HTML
-            -- 'astro',       -- Astro components
-            -- 'eruby',       -- Ruby ERB templates
-            -- 'ejs',         -- Embedded JS templates
-            -- 'heex',        -- Phoenix/Elixir templates
+        opts = {
+            enable_close = true,
+            enable_rename = true,
+            enable_close_on_slash = false
         },
+        per_filetype = {
+            ["html"] = {
+                enable_close = true
+            }
+        }
     },
 }
