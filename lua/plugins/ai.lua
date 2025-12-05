855c44898c66dd082a27258d0e1c2024d156db58
Aryan Binazir <abinazir@gmail.com>
2025-12-05 15:54:04 -0500

    Disable by default

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -28,38 +28,38 @@ return {
   --     end, { desc = "Toggle Copilot" })
   --   end,
   -- },
-  {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<S-Tab>",
-          accept_word = "<C-w>",
-          clear_suggestion = "<C-e>",
-        },
-      })
-
-      local api = require("supermaven-nvim.api")
-      vim.api.nvim_create_autocmd("VimEnter", {
-        callback = function()
-          vim.defer_fn(function()
-            api.start()
-            print("se")
-          end, 100)
-        end,
-      })
-
-      vim.keymap.set("n", "<leader>sm", function()
-        if api.is_running() then
-          api.stop()
-          print("sd")
-        else
-          api.start()
-          print("se")
-        end
-      end, { desc = "Toggle SM" })
-    end,
-  },
+  -- {
+  --   "supermaven-inc/supermaven-nvim",
+  --   config = function()
+  --     require("supermaven-nvim").setup({
+  --       keymaps = {
+  --         accept_suggestion = "<S-Tab>",
+  --         accept_word = "<C-w>",
+  --         clear_suggestion = "<C-e>",
+  --       },
+  --     })
+  --
+  --     local api = require("supermaven-nvim.api")
+  --     vim.api.nvim_create_autocmd("VimEnter", {
+  --       callback = function()
+  --         vim.defer_fn(function()
+  --           api.start()
+  --           print("se")
+  --         end, 100)
+  --       end,
+  --     })
+  --
+  --     vim.keymap.set("n", "<leader>sm", function()
+  --       if api.is_running() then
+  --         api.stop()
+  --         print("sd")
+  --       else
+  --         api.start()
+  --         print("se")
+  --       end
+  --     end, { desc = "Toggle SM" })
+  --   end,
+  -- },
   {
     "coder/claudecode.nvim",
     opts = {
