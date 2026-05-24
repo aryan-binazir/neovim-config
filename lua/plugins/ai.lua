968e305841b4963ae475154be611ee8d9317ce7b
Aryan Binazir <abinazir@gmail.com>
2026-05-24 18:26:51 -0400

    chore: make claudecode configurable

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"coder/claudecode.nvim",
+		enabled = vim.g.claudecode_enabled ~= false,
 		opts = {
 			terminal = {
 				provider = "none", -- no UI actions; server + tools remain available
